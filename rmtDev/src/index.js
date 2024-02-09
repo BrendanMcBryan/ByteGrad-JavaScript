d961aa88586c5e7d0f1b6bfc74a4d96dfbd41ee9
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-02-09 14:09:55 -0800

    Webpack install and production project working

---
+++ rmtDev/src/index.js
@@ -0,0 +1,12 @@
+import './component/Bookmarks.js';
+import './component/Error.js';
+import './component/JobList.js';
+import './component/JobDetails.js';
+import './component/Router.js';
+import './component/Search.js';
+import './component/Storage.js';
+import './component/Spinner.js';
+import './component/Sorting.js';
+import './component/Pagination.js';
+
+import './index.css';
